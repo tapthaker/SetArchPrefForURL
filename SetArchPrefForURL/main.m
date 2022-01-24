7ba67db40584895579641e17638aec97a445add4
Tapan Thaker <tapanh.@uber.com>
2022-01-24 11:34:03 -0800

    Initial Commit

---
+++ SetArchPrefForURL/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SetArchPrefForURL
+//
+//  Created by Tapan Thaker on 1/24/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
